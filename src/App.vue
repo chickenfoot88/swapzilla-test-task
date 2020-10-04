f11bc4327f05eca94b8ac78af2956200d6e6f36a
chickenfoot88 <chikenfoot88@gmail.com>
2020-10-04 14:34:26 +0300

    Add layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
-  <v-app></v-app>
+  <v-app class="open-sans">
+    <div class="container">
+      <v-btn depressed color="primary" @click="showModal">Open Modal</v-btn>
+    </div>
+    <fund-acc-modal :visible.sync="isModalVisible"/>
+  </v-app>
 </template>
 
 <script>
+import FundAccModal from '@/components/FundAccModal'
+
 export default {
   name: 'App',
-  components: {},
-  data(){},
-};
+  components: {
+    FundAccModal
+  },
+  data() {
+    return {
+      isModalVisible: true
+    }
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true
+    }
+  }
+}
 </script>
+<style>
+body .open-sans {
+  font-family: 'Open Sans', sans-serif;
+}
+
+.container {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+</style>
