5f844af710b0144456e05b40b737a6b23399f908
chickenfoot88 <chikenfoot88@gmail.com>
2020-10-04 14:34:06 +0300

    Register base components

--- src/global-components.js
+++ src/global-components.js
@@ -3,6 +3,11 @@ import Vue from 'vue'
 const components = {
   'BaseModal':   () => import('@/components/BaseComponents/BaseModal'),
   'BaseTabs':   () => import('@/components/BaseComponents/BaseTabs'),
+  'BaseInput':   () => import('@/components/BaseComponents/BaseInput'),
+  'BaseSelect':   () => import('@/components/BaseComponents/BaseSelect'),
+  'BaseChip':   () => import('@/components/BaseComponents/BaseChip'),
+  'BaseInputWithAmount':   () => import('@/components/BaseComponents/BaseInputWithAmount'),
+  'TransitionExpand':   () => import('@/components/Utility/TransitionExpand')
 }
 
 Object.entries(components).forEach(([name, component]) => Vue.component(name, component))
