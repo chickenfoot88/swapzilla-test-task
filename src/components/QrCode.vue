e5f8fa431948d7de0c7e59f3ac0190089c614d78
chickenfoot88 <chikenfoot88@gmail.com>
2020-10-04 14:33:49 +0300

    Add base qr code component

---
+++ src/components/QrCode.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="qr-code text-center">
+    <img src="@/assets/demo/qr.svg" class="mb-3"/>
+    <span>FGBfvcnkfdnvkk74365832ybvcvdfvfvb</span>
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+<style>
+.qr-code {
+  min-height: 155px;
+}
+
+@media (max-width: 480px) {
+  span {
+    font-size: 14px;
+  }
+}
+
+</style>
