f2f18bedda6e6ba6446405e26eef9a876d27c5ca
chickenfoot88 <chikenfoot88@gmail.com>
2020-10-04 14:31:31 +0300

    Add utility and animation styles

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import './global-components'
+import '@/styles/utility.css'
+import '@/styles/animation.css'
 
 Vue.config.productionTip = false
 
